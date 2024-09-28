8569280792797f9e2d0e33231d489fbe68cb1cd1
Jacob Zhang <jacob-z@live.de>
2024-09-28 19:02:38 +0200

    Generate pitch accent SVG

--- card_templates/common.js
+++ card_templates/common.js
@@ -8,79 +8,142 @@ for (const pitchAccent of document.querySelectorAll(
     pitchAccent.dataset.reading.length > 0
       ? pitchAccent.dataset.reading
       : lemma;
-  const readingCharArr = reading.split("");
-  const pitchAccentCharArr = pitchAccent.textContent.split("");
 
-  if (
-    pitchAccent.textContent.replaceAll("(", "").replaceAll(")", "").length !==
-    readingCharArr.length
-  ) {
-    console.log("Pitch accent notation and reading length mismatch");
-    pitchAccent.innerHTML = readingCharArr.join("");
+  const { result, indices } = removeParentheses(pitchAccent.textContent);
+  if (result.length !== reading.length) {
+    console.log(
+      "Pitch accent notation and reading length mismatch",
+      result,
+      reading
+    );
     continue;
   }
 
   pitchAccent.innerHTML = "";
-  let isUnvoiced = false;
-  while (readingCharArr.length > 0) {
-    const pitch = pitchAccentCharArr.shift();
-    if (pitch === "(") {
-      isUnvoiced = true;
-      continue;
-    }
-    if (pitch === ")") {
-      isUnvoiced = false;
-      continue;
-    }
+  pitchAccent.appendChild(createSvgWithPitchAccent(reading, result, indices));
+}
 
-    const char = readingCharArr.shift();
-    let charClass = "";
-    if (pitch === "^") {
-      charClass = "accent_top";
-    } else if (pitch === "/") {
-      charClass = "accent_bottom accent_rising";
-    }else if (pitch === "\\") {
-      charClass = "accent_top accent_falling";
-    } else {
-      charClass = "accent_bottom";
-    }
+function removeParentheses(str) {
+  let indices = [];
+  let result = "";
 
-    let newHtml = `<span class="${charClass}">${char}</span>`;
-    if (isUnvoiced) {
-      newHtml = `<span class="unvoiced">${newHtml}</span>`;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      indices.push(i);
+    } else if (str[i] !== ")") {
+      result += str[i];
     }
-    pitchAccent.innerHTML += newHtml;
   }
+
+  return { result, indices };
+}
+
+function createSvgWithPitchAccent(textInput, pitchAccent, unvoiced) {
+  const xmlns = "http://www.w3.org/2000/svg";
+  const charWidth = 19;
+  const svgHeight = 27;
+  const svgWidth = textInput.length * charWidth + charWidth / 2;
+  const lineY = { top: 2, bottom: 25 };
+  const curveControl = 3.5;
+
+  const svg = document.createElementNS(xmlns, "svg");
+  svg.setAttribute("viewBox", `0 0 ${svgWidth} ${svgHeight}`);
+  svg.setAttribute("height", "1.5em");
+
+  const style = document.createElementNS(xmlns, "style");
+  style.textContent = `.pitch-char { font-size: ${charWidth}px; } .pitch-line { fill: none; opacity: 0.6 }`;
+  svg.appendChild(style);
+
+  for (let i = 0; i < textInput.length; i++) {
+    const textElement = document.createElementNS(xmlns, "text");
+    textElement.setAttribute("x", (i * charWidth + charWidth / 2).toString());
+    textElement.setAttribute("y", "20");
+    textElement.setAttribute("class", "pitch-char");
+    textElement.setAttribute("text-anchor", "middle");
+    textElement.textContent = textInput[i];
+    svg.appendChild(textElement);
+
+    if (unvoiced.includes(i)) {
+      const circle = document.createElementNS(xmlns, "circle");
+      circle.setAttribute("cx", (i * charWidth + charWidth / 2).toString());
+      circle.setAttribute("cy", "12.5");
+      circle.setAttribute("r", "10.5");
+      circle.setAttribute("fill", "none");
+      circle.setAttribute("opacity", "0.5");
+      circle.setAttribute("stroke-dasharray", "2,2");
+      svg.appendChild(circle);
+    }
+  }
+
+  let pathData = [];
+  let currentY =
+    pitchAccent[0] === "/" || pitchAccent[0] === "." ? lineY.bottom : lineY.top;
+
+  pathData.push(`M0,${currentY}`);
+
+  for (let i = 0; i < textInput.length; i++) {
+    const x = (i + 1) * charWidth;
+    const accent = pitchAccent[i];
+    const nextAccent = pitchAccent[i + 1];
+
+    if (accent === "/" || accent === "\\") {
+      const targetY = accent === "/" ? lineY.top : lineY.bottom;
+      pathData.push(`L${x - curveControl},${currentY}`);
+      pathData.push(
+        `C${x},${currentY} ${x},${targetY} ${x + curveControl},${targetY}`
+      );
+      currentY = targetY;
+    } else if (accent === "^") {
+      currentY = lineY.top;
+      pathData.push(`L${x},${currentY}`);
+    } else {
+      currentY = lineY.bottom;
+      pathData.push(`L${x},${currentY}`);
+    }
+
+    if (nextAccent === "^") currentY = lineY.top;
+    if (nextAccent === ".") currentY = lineY.bottom;
+  }
+
+  const path = document.createElementNS(xmlns, "path");
+  path.setAttribute("d", pathData.join(" "));
+  path.setAttribute("class", "pitch-line");
+  svg.appendChild(path);
+
+  return svg;
 }
 
 function addFurigana(text, kanjiHighlightMap = {}) {
-  return text.replaceAll(
-    /\s?([^\s\p{P}]+?)\[([^\s\p{P}]+?)\]/gu,
-    (match, kanji, furigana) => {
-      const kanjiMatch = Object.keys(kanjiHighlightMap).find((key) => {
-        return kanji.includes(key) ? key : null;
-      });
-      if (kanjiMatch) {
-        const reading = kanjiMatch === kanji ? furigana : kanjiHighlightMap[kanjiMatch];
-        if (furigana.includes(reading)) {
-          kanji = kanji.replaceAll(kanjiMatch, reading);
-          if (furigana === reading) {
-            return kanji;
+  return text
+    .replaceAll(
+      /\s?([^\s\p{P}]+?)\[([^\s\p{P}]+?)\]/gu,
+      (match, kanji, furigana) => {
+        const kanjiMatch = Object.keys(kanjiHighlightMap).find((key) => {
+          return kanji.includes(key) ? key : null;
+        });
+        if (kanjiMatch) {
+          const reading =
+            kanjiMatch === kanji ? furigana : kanjiHighlightMap[kanjiMatch];
+          if (furigana.includes(reading)) {
+            kanji = kanji.replaceAll(kanjiMatch, reading);
+            if (furigana === reading) {
+              return kanji;
+            }
           }
         }
+        return `${
+          kanji.startsWith("　") ? "　" : ""
+        }<ruby><span class="rb">${kanji.trim()}</span><span class="rt">${furigana}</span></ruby>`;
       }
-      return `${
-        kanji.startsWith("　") ? "　" : ""
-      }<ruby><span class="rb">${kanji.trim()}</span><span class="rt">${furigana}</span></ruby>`;
-    }
-  ).replaceAll('> <', '><');
+    )
+    .replaceAll("> <", "><");
 }
 
 function togglePopup(ruby, state) {
   const rt = ruby.querySelector(".rt");
   let shouldTransition = state;
 
-  rt.addEventListener('transitionend', () => {
+  rt.addEventListener("transitionend", () => {
     if (!shouldTransition) {
       return;
     }
@@ -89,10 +152,14 @@ function togglePopup(ruby, state) {
     requestAnimationFrame(() => {
       if (rt.getBoundingClientRect().left < 0) {
         console.log(rt.getBoundingClientRect().x);
-        rt.style.transform = `translateX(calc(-50% - ${rt.getBoundingClientRect().left}px))`;
+        rt.style.transform = `translateX(calc(-50% - ${
+          rt.getBoundingClientRect().left
+        }px))`;
       } else if (rt.getBoundingClientRect().right > window.innerWidth) {
         console.log(rt.getBoundingClientRect().right, window.innerWidth);
-        rt.style.transform = `translateX(calc(-50% + ${window.innerWidth - rt.getBoundingClientRect().right}px))`;
+        rt.style.transform = `translateX(calc(-50% + ${
+          window.innerWidth - rt.getBoundingClientRect().right
+        }px))`;
       }
     });
   });
@@ -120,7 +187,7 @@ function enableRuby(el = document) {
         event.stopPropagation();
         togglePopup(rb.parentElement, false);
         rt.onclick = null;
-      }
+      };
     };
     rb.ondblclick = (event) => {
       event.stopPropagation();
@@ -146,12 +213,24 @@ function formatDefinition() {
         /([^,;]+)/g,
         (match) => `<span class="no-break">${match}</span>`
       );
-      text = text.replaceAll('<span class="no-break"> ', ' <span class="no-break">');
-      text = text.replaceAll('</span>, <span class="no-break">', ',</span> <span class="no-break">')
-      text = text.replaceAll('</span>; <span class="no-break">', ';</span> <span class="no-break">')
+      text = text.replaceAll(
+        '<span class="no-break"> ',
+        ' <span class="no-break">'
+      );
+      text = text.replaceAll(
+        '</span>, <span class="no-break">',
+        ',</span> <span class="no-break">'
+      );
+      text = text.replaceAll(
+        '</span>; <span class="no-break">',
+        ';</span> <span class="no-break">'
+      );
     }
 
-    text = text.replaceAll(/\(-?(.*?)-?\)/g, '<span class="pre-suffix">$1</span>');
+    text = text.replaceAll(
+      /\(-?(.*?)-?\)/g,
+      '<span class="pre-suffix">$1</span>'
+    );
     text = text.replaceAll(/\[(.*?)\]/g, '<span class="grammar">$&</span>');
 
     def.innerHTML = text;
